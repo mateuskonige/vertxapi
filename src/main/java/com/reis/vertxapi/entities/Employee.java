8ca5750590d55077b00f4f239f12bb8b1747c4ef
Mateus Reis <mateuskonige@outlook.com>
2024-02-21 15:05:41 -0300

    feat: Employee entity

---
+++ src/main/java/com/reis/vertxapi/entities/Employee.java
@@ -0,0 +1,54 @@
+package com.reis.vertxapi.entities;
+
+import java.util.Objects;
+
+public class Employee {
+
+	private String id;
+	private String name;
+
+	public Employee(String id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name);
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + "]";
+	}
+
+}
